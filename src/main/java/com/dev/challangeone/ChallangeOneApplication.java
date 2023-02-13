bcf5d4d265f5044ada41b958623969110dbb231f
unknown <jarazethalba@gmail.com>
2023-02-13 07:51:23 -0600

    Initial commit

---
+++ src/main/java/com/dev/challangeone/ChallangeOneApplication.java
@@ -0,0 +1,13 @@
+package com.dev.challangeone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallangeOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallangeOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/challangeone/ChallangeOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.challangeone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallangeOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
